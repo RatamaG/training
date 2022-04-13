351d430f0b86f11a5a17052f09e04faff451ecb3
RafaT <rafaeltamarag1@gmail.com>
2022-04-13 18:45:33 -0500

    creation of the letters and the letterz

---
+++ cmd/letraz/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("enter a number")
+
+		return
+	}
+	
+	name := os.Args[1]
+	dim, err := strconv.Atoi(name)
+	if err != nil {
+		fmt.Println("enter a number, letters are not allowed")
+	}
+	for i := 0; i < dim; i++ {
+		fmt.Print("*")
+	}
+	fmt.Println()
+	for i := 0; i < dim-2; i++ {
+
+		for j :=1; j <= dim-i-2; j++ {
+			fmt.Print(" ")
+		}
+
+		fmt.Print("*")
+		fmt.Println()
+	}
+
+	for i := 0; i < dim; i++ {
+		fmt.Print("*")
+	}
+
+}
