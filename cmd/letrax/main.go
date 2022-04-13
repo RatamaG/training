f37e12346f384286ed8e90cb974182ed6a4be4e5
RafaT <rafaeltamarag1@gmail.com>
2022-04-13 11:15:24 -0500

    Cambiando de forma la letra x, haciendola con otro metodo planteado

--- cmd/letrax/main.go
+++ cmd/letrax/main.go
@@ -17,25 +17,28 @@ func main() {
 	if err != nil {
 		fmt.Println("ingresa un numero, letras no son compatibles")
 	}
-	sb := 0
-	for i := 0; i < dim-1; i++ {
-		for j := 0; j < sb; j++ {
+	//Primera parte
+	for i := dim/2; i <= dim-1; i++ {
+		for j := 1; j <= i; j++ {
 			fmt.Print(" ")
 		}
-
 		fmt.Print("*")
-		sm := dim - (i+1)*2
-		if sm < 0 {
-			sm = sm * -1
+		for j := 1; j <= dim-i; j++ {
+			fmt.Print("  ")
 		}
-		for j := 0; j < sm; j++ {
+		fmt.Print("*")
+		fmt.Println()
+	}
+	//segnda parte
+	for i := 0; i <= dim/2-1; i++ {
+		for j := 1; j <= dim-i; j++ {
 			fmt.Print(" ")
 		}
-		fmt.Print("*", "\n")
-		if i >= ((dim / 2) - 1) {
-			sb--
-		} else {
-			sb++
+		fmt.Print("*")
+		for j := 1; j <= i; j++ {
+			fmt.Print("  ")
 		}
+		fmt.Print("*")
+		fmt.Println()
 	}
 }
